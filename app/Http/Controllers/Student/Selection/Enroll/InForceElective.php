9b8627173428fe90ad1d923aee7f9ae125596493
Ernest <iattempt.net@gmail.com>
2017-02-19 16:20:53 +0800

    update

---
+++ app/Http/Controllers/Student/Selection/Enroll/InForceElective.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Http\Controllers\Student\Selection\Enroll;
+
+use Illuminate\Http\Request;
+use App\Http\Controllers\Controller;
+
+class InForceElective extends Controller
+{
+    //
+}
