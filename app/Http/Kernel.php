b25766ee5c4f5cfd583fd2d38a33008f96d50926
iattempt <iattempt.net@gmail.com>
2017-03-21 16:49:10 +0000

    update

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -56,8 +56,5 @@ class Kernel extends HttpKernel
         'can' => \Illuminate\Auth\Middleware\Authorize::class,
         'guest' => \App\Http\Middleware\RedirectIfAuthenticated::class,
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
-        'authority' => \App\Http\Middleware\Authority::class,
-        'student' => \App\Http\Middleware\Student::class,
-        'professor' => \App\Http\Middleware\Professor::class,
     ];
 }
--- resources/views/member/sign_in.blade.php
+++ resources/views/member/sign_in.blade.php
@@ -3,25 +3,21 @@
 <div class="container">
   <form action="/course_search" method="post">
     {{ csrf_field() }}
-    <div class="form-group row">
-      <label for="email" class="col-4 col-sm-2 col-form-label d-flex justify-content-center">Email</label>
-      <div class="col-8 col-sm-10">
+    <div class="form-group row d-flex justify-content-center">
+      <div class="col-12 col-sm-8 col-md-6 row my-4">
+        <h2 class="">登入</h2>
+        <hr class="col-12">
         <input type="email" class="form-control" id="authority" placeholder="account@example.com">
-      </div>
-    </div>
-    <div class="form-group row">
-      <label for="password" class="col-4 col-sm-2 col-form-label d-flex justify-content-center">Password</label>
-      <div class="col-8 col-sm-10">
-        <input type="password" class="form-control" id="password">
-      </div>
-    </div>
-    <div class="form-group row">
-      <div class="input-group col-12 d-flex justify-content-end">
-        <span class="input-group-addon">
-          <input type="checkbox" aria-label="Checkbox for following text input">
-        </span>
-        <span class="input-group-addon">Remember me</span>
-        <input type="submit" class="btn btn-primary" value="Sign in">
+        <input type="password" class="form-control my-2" id="password" placeholder="password">
+        <a href="#">忘記密碼?</a>
+        <hr class="col-12">
+        <div class="input-group d-flex justify-content-end">
+          <span class="input-group-addon">
+            <input type="checkbox" aria-label="Checkbox for following text input">
+          </span>
+          <span class="input-group-addon">Remember me</span>
+          <input type="submit" class="btn btn-primary" value="Sign in">
+        </div>
       </div>
     </div>
   </form>
