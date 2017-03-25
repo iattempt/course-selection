7b1d35fdb9e86d94d0fbb9ebd9476b41fb3003aa
iattempt <iattempt.net@gmail.com>
2017-03-25 16:38:31 +0000

    update

--- resources/views/schema/footer.blade.php
+++ resources/views/schema/footer.blade.php
@@ -1,18 +1,16 @@
 @extends('schema/nav')
 @section('footer')
-      <footer class="row fixed-bottom">
-        <span class="mx-auto">
-          &copy; 2017 Designed and maintained by the
-          <a href="https://github.com/iattempt">Ernest</a>
-        </span>
-        <div class="col-12"></div>
-        <span class="mx-auto">
-          &nbsp;with the help of
-          <a href="https://github.com/">the Github, inc.</a>
-        </span>
-      </footer>
-    </div>
-  </div>
+  <footer class="row fixed-bottom">
+    <span class="mx-auto">
+      &copy; 2017 Designed and maintained by the
+      <a href="https://github.com/iattempt">Ernest</a>
+    </span>
+    <div class="col-12"></div>
+    <span class="mx-auto">
+      &nbsp;with the help of
+      <a href="https://github.com/">the Github, inc.</a>
+    </span>
+  </footer>
 </body>
 </html>
 @endsection
--- resources/views/schema/header.blade.php
+++ resources/views/schema/header.blade.php
@@ -9,18 +9,14 @@
     @endsection
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
   </head>
-<body class="bd-docs container-fluid" data-spy="scroll" data-target=".bd-sidenav-active">
-  <div class="row mx-lg-1">
-    <div class="col-12">
-      <a id="skippy" class="sr-only sr-only-focusable" href="#content">
-        <div class="container">
-          <span class="skiplink-text">Skip to main content</span>
-        </div>
-      </a>
-      @section('main-nav')
-      @show
-      @section('main')
-      @show
-      @section('footer')
-      @show
+<body class="bd-docs" data-spy="scroll" data-target=".bd-sidenav-active">
+  <a id="skippy" class="sr-only sr-only-focusable" href="#content">
+    <span class="skiplink-text">Skip to main content</span>
+  </a>
+  @section('main-nav')
+  @show
+  @section('main')
+  @show
+  @section('footer')
+  @show
 
