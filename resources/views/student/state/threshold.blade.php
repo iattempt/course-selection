b3e217f6388f1752c7b8708ab1936f3ea30949cb
iattempt <iattempt.net>
2017-05-26 15:43:09 +0000

    update

--- resources/views/student/state/threshold.blade.php
+++ resources/views/student/state/threshold.blade.php
@@ -1,10 +1,6 @@
 @extends ('student/state')
 @section ('state')
-  <div class="row">
   @include ('student/state/threshold/overview')
-  </div>
   <hr class="my-1">
-  <div class="row">
   @include ('student/state/threshold/detail')
-  </div>
 @endsection
--- resources/views/student/state/threshold/detail.blade.php
+++ resources/views/student/state/threshold/detail.blade.php
@@ -1,53 +1,55 @@
 <a class="btn btn-primary col-12" data-toggle="collapse" href="#detail_threshold" aria-expanded="true" aria-controll="detail_threshold">
     詳細資訊
 </a>
-<div id="detail_threshold" class="collapse col-12">
-  @foreach ($general->threshold as $state_key => $state_value)
-    <a class="btn btn-primary col-12" data-toggle="collapse" href="#threshold_{{$state_key}}" aria-expanded="true" aria-controll="threshold_{{$state_key}}">
-        {{$state_key}}
-    </a>
-    <div id="threshold_{{$state_key}}" class="collapse col-12">
-      @foreach ($state_value as $type_key => $type_value)
-        <div class="row">
-          <div class="col-4 bg-info">
-            {{$type_key}}
-          </div>
-          <div class="col-12">
-            <div class="row">
-              <div class="col-6">
-                課程名稱
+<div id="detail_threshold" class="collapse container">
+  <div class="row">
+    @foreach ($general->threshold as $state_key => $state_value)
+      <a class="btn btn-primary col-12" data-toggle="collapse" href="#threshold_{{$state_key}}" aria-expanded="true" aria-controll="threshold_{{$state_key}}">
+          {{$state_key}}
+      </a>
+      <div id="threshold_{{$state_key}}" class="collapse col-12">
+          <div class="row">
+            <div class="col-12">
+              <div class="row">
+                <div class="col-5">
+                  課程名稱
+                </div>
+                <div class="col-4">
+                  學期
+                </div>
+                <div class="col-3">
+                  學分
+                </div>
               </div>
-              <div class="col-3">
-                學年 - 學期
-              </div>
-              <div class="col-1">
-                學分
-              </div>
-              <hr class="col-12 my-0">
             </div>
           </div>
-          <div class="col-12">
-          @foreach ($type_value as $key => $course)
-            @if ($loop->index%2)
-            <div class="row">
-            @else
-            <div class="row bg-faded">
-            @endif
-              <div class="col-6">
-                {{$course['course_name']}}
-              </div>
-              <div class="col-2">
-                {{$course['year']}} - {{$course['semester']}}
-              </div>
-              <div class="col-1">
-                {{$course['credit']}}
-              </div>
-              <hr class="col-12 my-0">
+        @foreach ($state_value as $type_key => $type_value)
+          <div class="row">
+            <div class="col-4 bg-info align-self-start">
+              {{$type_key}}
             </div>
-          @endforeach
+            <div class="col-12">
+            @foreach ($type_value as $key => $course)
+              @if ($loop->index%2)
+              <div class="row">
+              @else
+              <div class="row bg-faded">
+              @endif
+                <div class="col-5">
+                  {{$course['course_name']}}
+                </div>
+                <div class="col-4">
+                  {{$course['year']}} - {{$course['semester']}}
+                </div>
+                <div class="col-3">
+                  {{$course['credit']}}
+                </div>
+              </div>
+            @endforeach
+            </div>
           </div>
-        </div>
-      @endforeach
-    </div>
-  @endforeach
+        @endforeach
+      </div>
+    @endforeach
+  </div>
 </div>
