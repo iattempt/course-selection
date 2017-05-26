9ff948aa449f0100e69d19c38641262f531645c2
iattempt <iattempt.net@gmail.com>
2017-05-26 12:35:28 +0000

    update

--- resources/views/student/state/threshold.blade.php
+++ resources/views/student/state/threshold.blade.php
@@ -1,6 +1,10 @@
 @extends ('student/state')
 @section ('state')
+  <div class="row">
   @include ('student/state/threshold/overview')
+  </div>
   <hr class="my-1">
+  <div class="row">
   @include ('student/state/threshold/detail')
+  </div>
 @endsection
--- resources/views/student/state/threshold/detail.blade.php
+++ resources/views/student/state/threshold/detail.blade.php
@@ -9,8 +9,22 @@
     <div id="threshold_{{$state_key}}" class="collapse col-12">
       @foreach ($state_value as $type_key => $type_value)
         <div class="row">
-          <div class="col-2 bg-info">
+          <div class="col-4 bg-info">
             {{$type_key}}
+          </div>
+          <div class="col-12">
+            <div class="row">
+              <div class="col-6">
+                課程名稱
+              </div>
+              <div class="col-3">
+                學年 - 學期
+              </div>
+              <div class="col-1">
+                學分
+              </div>
+              <hr class="col-12 my-0">
+            </div>
           </div>
           <div class="col-12">
           @foreach ($type_value as $key => $course)
@@ -19,14 +33,11 @@
             @else
             <div class="row bg-faded">
             @endif
-              <div class="col-4">
+              <div class="col-6">
                 {{$course['course_name']}}
               </div>
               <div class="col-2">
-                {{$course['year']}}
-              </div>
-              <div class="col-1">
-                {{$course['semester']}}
+                {{$course['year']}} - {{$course['semester']}}
               </div>
               <div class="col-1">
                 {{$course['credit']}}
