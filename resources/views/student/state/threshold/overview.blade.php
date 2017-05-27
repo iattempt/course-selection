2420e75368b647199de7071ebe1cbf480bfa7632
iattempt <iattempt.net>
2017-05-27 15:06:28 +0000

    update

--- resources/views/student/state/threshold/overview.blade.php
+++ resources/views/student/state/threshold/overview.blade.php
@@ -2,21 +2,24 @@
   整體狀態
 </a>
 <div id="overview_threshold" class="collapse container show my-3">
-  <div class="row justify-content-center mx-5">
+  <div class="row justify-content-center mx-1 mx-md-5">
     <div class="col-12">
       <div class="row">
         @foreach ($general->credit['Canvas'] as $type_key => $type_value)
-        <div class="col-6 offset-lg-1 col-lg-2">
+        <div class="col-4 offset-lg-1 col-lg-2">
           <div class="row">
-            <canvas id="{{$type_key}}" width="100" height="100"></canvas>
+            <canvas id="{{$type_key}}" width="75" height="75"></canvas>
           </div>
           <div class="row">
-            {{$type_key}} [{{$general->credit['學分狀態'][$type_key]}}]
+            {{$type_key}}
+          </div>
+          <div class="row alert-danger">
+            [{{$general->credit['學分狀態'][$type_key]}}]
           </div>
         </div>
-        <div class="col-6 col-lg-3">
+        <div class="col-8 col-lg-3">
         @foreach ($general->credit['Canvas'][$type_key] as $state_key => $state_value)
-          <div>
+          <div class="row">
           <canvas id="{{$type_key}}{{$state_key}}" width="25" height="25"></canvas>
             {{$state_key}}:{{ ($state_value['end']-$state_value['start'])!=0 ? (number_format(($state_value['end']-$state_value['start'])*50, 2)).'%' : '無'}}
           </div>
@@ -42,8 +45,8 @@ window.onload = function(){
       //+1.5 是為了使得終點為12點鐘
       var start = canvas_value[type][state]['start']+1.5;
       var end = canvas_value[type][state]['end']+1.5;
-      ctx.arc(50, 50, 24, start * Math.PI, end * Math.PI);
-      ctx.lineWidth=50;
+      ctx.arc(37.5, 37.5, 18.5, start * Math.PI, end * Math.PI);
+      ctx.lineWidth=37.5;
       var ctext = document.getElementById(type+state).getContext("2d");
       if (state == '未修過') {
         ctx.strokeStyle='rgb(255, 0, 0)';
