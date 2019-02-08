da947eeda73d6a38c47f186625cc74eaac072822
Ernest <iattempt.net@gmail.com>
2019-02-08 13:12:49 +0800

    Update Professor.php

--- app/Http/Middleware/Professor.php
+++ app/Http/Middleware/Professor.php
@@ -16,8 +16,8 @@ class Professor
      */
     public function handle($request, Closure $next)
     {
-        if (Auth::check()
-            && Auth::user()->isProfessor())
+        if (Auth::check() &&
+            Auth::user()->isProfessor())
             return $next($request);
 
         return redirect('login');
